8ee864d559163ad63532b599d8ffa67b28ee2a0e
GGolfz <wisarutgolf11@gmail.com>
2022-04-12 22:30:14 +0700

    add: combine :sparkles:

--- similarities.py
+++ similarities.py
@@ -14,20 +14,12 @@ def get_data(audio):
     arr = librosa.core.resample(arr, audio.frame_rate, 22050, res_type="kaiser_best")
     S = librosa.feature.melspectrogram(arr, sr=22050, n_mels=256)
     mels = np.log(S + 1e-9)
-    bpm = round(librosa.beat.tempo(y=arr)[0],2)
-    normalized_bpm = normalize_bpm(bpm)
     normalized_mel = normalize_mel(mels).flatten()
-    weighted_bpm = np.array([normalized_bpm] * int(len(normalized_mel) * 2))
-    return np.concatenate((weighted_bpm, normalized_mel))
-
-def normalize_bpm(bpm):
-    # Max is 200 bpm, min is 40 bpm
-    return (bpm - 40) / 160 
+    return normalized_mel
 
 def normalize_mel(audio):
     # Min is -25, max is 10
     return (audio + 25) / 35
-
 
 def get_similarity(audio1, audio2):
     audio1 = get_data(audio1[-mschunk:])
@@ -44,36 +36,51 @@ def compare_two_audio(audio1, audio2):
     # print("Similarity =", similarity)
     return similarity
 
-def combine(part1, part2):
-    combined = part1[:-1000] + part1[-1000:].overlay(part2[:1000]) + part2[1000:]
-    return combined
-
 def find_most_similar_songs(current_song, mood):
     next_song_lists = np.random.choice(list(map(lambda x: f"extract/{mood}/" + x,os.listdir(f"extract/{mood}"))),size = 100)
-
     mapper = {}
-    start_time = time.time()
-    i=0
     for next_song in next_song_lists:
         if current_song != next_song:
             similarity = compare_two_audio(current_song, next_song)
             mapper[next_song] = similarity
-        i+=1
-        if i % 10 == 0:
-            print(i,"/",len(next_song_lists),sep='')
+    return max(mapper, key=mapper.get)
 
-    end_time = time.time()
-    print("Wall time:", round(end_time - start_time,2),"seconds")
-    print("Current song:", current_song)
-    n = 5
-    i = 0
-    print(f"Top {n} most similar songs:")
-    for key, value in sorted(mapper.items(), key=lambda item: item[1], reverse=True)[:n]:
-        print(key, value)
-        i+=1
-        audio1 = AudioSegment.from_wav(current_song)
-        audio2 = AudioSegment.from_wav(key)
-        combined = combine(audio1, audio2)
-        combined.export(f"combined_{i}.wav", format="wav")
+def postprocessing(audio, time):
+    five_second = 5 * 1000
+    audio = trim_audio(audio,time)
+    audio = match_target_amplitude(audio, -20.0)
+    audio = audio.fade_in(five_second).fade_out(five_second)
+    return audio
+def match_target_amplitude(sound, target_dBFS):
+    change_in_dBFS = target_dBFS - sound.dBFS
+    return sound.apply_gain(change_in_dBFS)
 
-find_most_similar_songs("extract/Chill/Good As I Wanted_3.wav", "Chill")
+def trim_audio(audio,time):
+    audio = audio[:time * 1000]
+    return audio
+
+def combine(part1, part2):
+    combined = part1[:-500] + part1[-500:].overlay(part2[:500]) + part2[500:]
+    return combined
+
+def combine_all(audio):
+    combined = audio[0]
+    combined = AudioSegment.from_wav(combined)
+    for part in audio[1:]:
+        part = AudioSegment.from_wav(part)
+        combined = combine(combined, part)
+    return combined
+
+def create_list_of_song(mood, n = 5):
+    current_song = np.random.choice(list(map(lambda x: f"extract/{mood}/" + x,os.listdir(f"extract/{mood}"))),size = 1)[0]
+    song_list = [current_song]
+    for _ in range(n):
+        next_song = find_most_similar_songs(current_song, mood)
+        song_list.append(next_song)
+        current_song = next_song
+    print(song_list)
+    combined = combine_all(song_list)
+    combined = postprocessing(combined, 300)
+    combined.export("combined.wav", format="wav")
+
+create_list_of_song("Chill")
